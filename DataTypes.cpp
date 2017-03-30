e361e4e7cca1de4fb7f83c175bad069fdeefb515
shivamcode <shivam.kagalkar@gmail.com>
2017-03-31 00:55:53 +0530

    Create DataTypes.cpp
    In this program we demonstrate mostly used data types in c++.

---
+++ DataTypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  float b;
+  char c;
+  double d;
+  cout<<"\n Enter Number:";
+  cin>>a;
+  cout<<"\n Enter Character :";
+  cin>>c;
+  cout<<"\n Enter Double type Number :";
+  cin>>d;
+  cout<<"\n Integer Number :"<<a<<"\n Float Formation :"<<b<<"\n Character :"<<c<<"\n Double Type Number :"<<d;
+  return 0;
+}
